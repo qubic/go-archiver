d225753c155bb8d45befb3b48a6f8e7fa3cc2e64
0xluk <luk@luktech.dev>
2024-03-11 20:05:36 +0200

    added qchain tests

---
+++ validator/qchain/qchain_test.go
@@ -0,0 +1,28 @@
+package qchain
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestQChain_Digest(t *testing.T) {
+	qChain := QChain{
+		ComputorIndex:                 1,
+		Epoch:                         20,
+		Tick:                          30,
+		Millisecond:                   40,
+		Second:                        50,
+		Minute:                        60,
+		Hour:                          70,
+		Day:                           80,
+		Month:                         90,
+		Year:                          10,
+		PreviousResourceTestingDigest: 13423432,
+		PreviousSpectrumDigest:        [32]byte{},
+		PreviousUniverseDigest:        [32]byte{},
+		PreviousComputerDigest:        [32]byte{},
+		TxDigest:                      [32]byte{},
+	}
+	_, err := qChain.Digest()
+	require.NoError(t, err)
+}
