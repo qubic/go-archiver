36d53263b852b5b792ef532214b1a34b69714dda
0xluk <luk@luktech.dev>
2024-12-23 17:01:03 +0200

    switched GetStatus to only return empty ticks for current epoch instead of all epochs; part of the optimisation process

--- rpc/rpc_server.go
+++ rpc/rpc_server.go
@@ -370,14 +370,9 @@ func (s *Server) GetStatus(ctx context.Context, _ *emptypb.Empty) (*protobuff.Ge
 		return nil, status.Errorf(codes.Internal, "getting processed tick intervals")
 	}
 
-	var epochs []uint32
-	for epoch, _ := range lastProcessedTicksPerEpoch {
-		epochs = append(epochs, epoch)
-	}
-
-	emptyTicksForAllEpochs, err := s.store.GetEmptyTicksForEpochs(epochs)
+	emptyTickForCurrentEpoch, err := s.store.GetEmptyTicksForEpoch(lastProcessedTick.Epoch)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "getting empty ticks for all epochs: %v", err)
+		return nil, status.Errorf(codes.Internal, "getting empty ticks for current epoch: %v", err)
 	}
 
 	return &protobuff.GetStatusResponse{
@@ -385,7 +380,7 @@ func (s *Server) GetStatus(ctx context.Context, _ *emptypb.Empty) (*protobuff.Ge
 		LastProcessedTicksPerEpoch:     lastProcessedTicksPerEpoch,
 		SkippedTicks:                   skippedTicks.SkippedTicks,
 		ProcessedTickIntervalsPerEpoch: ptie,
-		EmptyTicksPerEpoch:             emptyTicksForAllEpochs,
+		EmptyTicksPerEpoch:             map[uint32]uint32{lastProcessedTick.Epoch: emptyTickForCurrentEpoch},
 	}, nil
 }
 
