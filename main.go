5af8635a95363a1395c497d0bb18ea4ca80a7f9b
mio <180114449+qubicmio@users.noreply.github.com>
2025-12-01 10:27:40 +0000

    Only startup and compact database

--- main.go
+++ main.go
@@ -2,27 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/ardanlabs/conf"
-	"github.com/cockroachdb/pebble"
-	grpcProm "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
-	"github.com/pkg/errors"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/collectors"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/qubic/go-archiver/processor"
-	"github.com/qubic/go-archiver/rpc"
-	"github.com/qubic/go-archiver/store"
-	"github.com/qubic/go-archiver/validator/tick"
-	qubic "github.com/qubic/go-node-connector"
-	"github.com/qubic/go-node-connector/types"
 	"log"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
 	"time"
+
+	"github.com/ardanlabs/conf"
+	"github.com/cockroachdb/pebble"
+	"github.com/pkg/errors"
+	"github.com/qubic/go-archiver/store"
 )
 
 const prefix = "QUBIC_ARCHIVER"
@@ -145,79 +136,84 @@ func run() error {
 	}
 	defer db.Close()
 
-	ps := store.NewPebbleStore(db, nil)
-
-	if cfg.Store.ResetEmptyTickKeys {
-		fmt.Printf("Resetting empty ticks for all epochs...\n")
-		err = tick.ResetEmptyTicksForAllEpochs(ps)
-		if err != nil {
-			return errors.Wrap(err, "resetting empty ticks keys")
-		}
-	}
-
-	err = tick.CalculateEmptyTicksForAllEpochs(ps)
+	err = db.Compact([]byte{0x00}, []byte{0xFF}, true)
 	if err != nil {
-		return errors.Wrap(err, "calculating empty ticks for all epochs")
+		return fmt.Errorf("compacting db: %w", err)
 	}
 
-	p, err := qubic.NewPoolConnection(qubic.PoolConfig{
-		InitialCap:         cfg.Pool.InitialCap,
-		MaxCap:             cfg.Pool.MaxCap,
-		MaxIdle:            cfg.Pool.MaxIdle,
-		IdleTimeout:        cfg.Pool.IdleTimeout,
-		NodeFetcherUrl:     cfg.Pool.NodeFetcherUrl,
-		NodeFetcherTimeout: cfg.Pool.NodeFetcherTimeout,
-		NodePort:           cfg.Qubic.NodePort,
-	})
-	if err != nil {
-		return errors.Wrap(err, "creating qubic pool")
-	}
+	//ps := store.NewPebbleStore(db, nil)
 
-	srvMetrics := grpcProm.NewServerMetrics(
-		grpcProm.WithServerCounterOptions(grpcProm.WithConstLabels(prometheus.Labels{"namespace": "archiver"})),
-	)
-	reg := prometheus.NewRegistry()
-	reg.MustRegister(srvMetrics)
-	reg.MustRegister(collectors.NewGoCollector())
+	//if cfg.Store.ResetEmptyTickKeys {
+	//	fmt.Printf("Resetting empty ticks for all epochs...\n")
+	//	err = tick.ResetEmptyTicksForAllEpochs(ps)
+	//	if err != nil {
+	//		return errors.Wrap(err, "resetting empty ticks keys")
+	//	}
+	//}
+	//
+	//err = tick.CalculateEmptyTicksForAllEpochs(ps)
+	//if err != nil {
+	//	return errors.Wrap(err, "calculating empty ticks for all epochs")
+	//}
+	//
+	//p, err := qubic.NewPoolConnection(qubic.PoolConfig{
+	//	InitialCap:         cfg.Pool.InitialCap,
+	//	MaxCap:             cfg.Pool.MaxCap,
+	//	MaxIdle:            cfg.Pool.MaxIdle,
+	//	IdleTimeout:        cfg.Pool.IdleTimeout,
+	//	NodeFetcherUrl:     cfg.Pool.NodeFetcherUrl,
+	//	NodeFetcherTimeout: cfg.Pool.NodeFetcherTimeout,
+	//	NodePort:           cfg.Qubic.NodePort,
+	//})
+	//if err != nil {
+	//	return errors.Wrap(err, "creating qubic pool")
+	//}
+	//
+	//srvMetrics := grpcProm.NewServerMetrics(
+	//	grpcProm.WithServerCounterOptions(grpcProm.WithConstLabels(prometheus.Labels{"namespace": "archiver"})),
+	//)
+	//reg := prometheus.NewRegistry()
+	//reg.MustRegister(srvMetrics)
+	//reg.MustRegister(collectors.NewGoCollector())
 
-	rpcServer := rpc.NewServer(cfg.Server.GrpcHost, cfg.Server.HttpHost, cfg.Server.NodeSyncThreshold, cfg.Server.ChainTickFetchUrl, ps, p)
-	err = rpcServer.Start(srvMetrics.UnaryServerInterceptor())
-	if err != nil {
-		return errors.Wrap(err, "starting rpc server")
-	}
+	//rpcServer := rpc.NewServer(cfg.Server.GrpcHost, cfg.Server.HttpHost, cfg.Server.NodeSyncThreshold, cfg.Server.ChainTickFetchUrl, ps, p)
+	//err = rpcServer.Start(srvMetrics.UnaryServerInterceptor())
+	//if err != nil {
+	//	return errors.Wrap(err, "starting rpc server")
+	//}
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
 
-	arbitratorID := types.Identity(cfg.Qubic.ArbitratorIdentity)
-	arbitratorPubKey, err := arbitratorID.ToPubKey(false)
-	if err != nil {
-		return errors.Wrapf(err, "getting arbitrator public key from [%s]", cfg.Qubic.ArbitratorIdentity)
-	}
+	//arbitratorID := types.Identity(cfg.Qubic.ArbitratorIdentity)
+	//arbitratorPubKey, err := arbitratorID.ToPubKey(false)
+	//if err != nil {
+	//	return errors.Wrapf(err, "getting arbitrator public key from [%s]", cfg.Qubic.ArbitratorIdentity)
+	//}
 
-	proc := processor.NewProcessor(p, ps, cfg.Qubic.ProcessTickTimeout, arbitratorPubKey, cfg.Qubic.DisableTransactionStatusAddon)
-	procErrors := make(chan error, 1)
+	//proc := processor.NewProcessor(p, ps, cfg.Qubic.ProcessTickTimeout, arbitratorPubKey, cfg.Qubic.DisableTransactionStatusAddon)
+	//procErrors := make(chan error, 1)
 
 	// Start the service listening for requests.
-	go func() {
-		procErrors <- proc.Start()
-	}()
+	//go func() {
+	//	procErrors <- proc.Start()
+	//}()
 
-	pprofErrors := make(chan error, 1)
-
-	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{EnableOpenMetrics: true}))
-	go func() {
-		pprofErrors <- http.ListenAndServe(cfg.Server.ProfilingHost, nil)
-	}()
+	//pprofErrors := make(chan error, 1)
+	//
+	//http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{EnableOpenMetrics: true}))
+	//go func() {
+	//	pprofErrors <- http.ListenAndServe(cfg.Server.ProfilingHost, nil)
+	//}()
 
 	for {
 		select {
 		case <-shutdown:
 			return errors.New("shutting down")
-		case err := <-pprofErrors:
-			log.Println("Profiling error: %w", err)
-		case err := <-procErrors:
-			return errors.Wrap(err, "archiver error")
+			//case err := <-pprofErrors:
+			//	log.Println("Profiling error: %w", err)
+			//case err := <-procErrors:
+			//	return errors.Wrap(err, "archiver error")
 		}
 	}
 }
